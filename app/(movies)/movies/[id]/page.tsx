8e0144555bb5d05103cf93cc8850079b88a96771
YOONKY <lov012726@gmail.com>
2024-02-29 17:34:29 +0900

    add : dynamic metadata by generateMetadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,11 +1,23 @@
 import { Suspense } from "react"
-import MovieInfo from "../../../../components/movie-info"
+import MovieInfo, { getMovie } from "../../../../components/movie-info"
 import MovieVideo from "../../../../components/movie-videos"
 
+interface IParams{
+  params: { id:string };
+}
+
+export async function generateMetadata({
+  params:{id}
+}: IParams){
+  const movie = await getMovie(id)
+  return{
+    title:movie.title
+  }
+}
 
 export default async function MovieDetail ({
   params : {id},
-} : { params : {id: string}}){
+} : IParams){
   
   
   return (
