9231dc89945300e3ae33d303a03d6ea17b946c89
YOONKY <lov012726@gmail.com>
2024-02-29 18:16:29 +0900

    Fix : change export URL to API_URL

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,11 +6,11 @@ export const metadata : Metadata = {
     title :"HOME"
 }
 
-export const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
 async function getMovies(){
     // await new Promise(res => setTimeout(res, 1000))
-    const response = await fetch(URL);
+    const response = await fetch(API_URL);
     const json = await response.json()
     return json;
 }
